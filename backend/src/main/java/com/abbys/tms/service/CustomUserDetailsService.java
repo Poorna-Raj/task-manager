ff4d95547356b15894040b8bfd3e4fba4a5e1b7c
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-22 14:08:31 +0530

    implement the user details service interface

---
+++ backend/src/main/java/com/abbys/tms/service/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.abbys.tms.service;
+
+import com.abbys.tms.data.user.entity.User;
+import com.abbys.tms.data.user.repository.UserRepo;
+import com.abbys.tms.exception.NotFound;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(() -> new NotFound("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                getAuthority(user)
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthority(User user) {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
+        return List.of(authority);
+    }
+}
