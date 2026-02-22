7f05daa18726e191fc417b91c97c6576159ae161
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-22 14:09:33 +0530

    define the model class for the token pair requests

---
+++ backend/src/main/java/com/abbys/tms/data/user/dto/TokenPair.java
@@ -0,0 +1,13 @@
+package com.abbys.tms.data.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class TokenPair {
+    private String accessToken;
+    private String refreshToken;
+}
