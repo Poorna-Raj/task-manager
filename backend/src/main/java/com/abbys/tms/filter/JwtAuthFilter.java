535e3d55bc6145025ea07e8337cc2cbeab218963
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-22 14:12:07 +0530

    add the import statement for the jwt service

--- backend/src/main/java/com/abbys/tms/filter/JwtAuthFilter.java
+++ backend/src/main/java/com/abbys/tms/filter/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package com.abbys.tms.filter;
 
+import com.abbys.tms.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
