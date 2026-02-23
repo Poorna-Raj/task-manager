8b00dddbf68d4b2e2f5542ada1a7ee24e10f7ba3
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-23 21:01:13 +0530

    implement the repository interface for the task history

--- backend/src/main/java/com/abbys/tms/data/taskHistory/repository/TaskHistoryRepo.java
+++ backend/src/main/java/com/abbys/tms/data/taskHistory/repository/TaskHistoryRepo.java
@@ -3,5 +3,8 @@ package com.abbys.tms.data.taskHistory.repository;
 import com.abbys.tms.data.taskHistory.entity.TaskHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskHistoryRepo extends JpaRepository<TaskHistory,Long> {
+    List<TaskHistory> findByTaskIdOrderByChangedAtDesc(Long taskId);
 }
